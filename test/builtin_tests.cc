1735a55fa0bfabadd156f1409d732acb97d00603
Kyle Brown <picokyle@gmail.com>
2016-02-07 15:49:21 -0700

    organize builtin_tests

--- test/builtin_tests.cc
+++ test/builtin_tests.cc
@@ -2,26 +2,40 @@
 #include "builtins.hh"
 #include "scheme.hh"
 
-TEST(BuiltinTest, Addition) {
+// +
+
+TEST(AdditionTests, BasicAddition) {
     ASSERT_EQ(3, intValue(eval(parse("(+ 1 2)"))));
 }
 
-TEST(BuiltinTest, AdditionIdentity) {
+TEST(AdditionTests, Identity) {
     ASSERT_EQ(0, intValue(eval(parse("(+)"))));
 }
 
-TEST(BuiltinTest, AbsPositiveInput) {
-    ASSERT_EQ(1, intValue(eval(parse("(abs 1)"))));
+TEST(AdditionTests, NonIntegralThrows) {
+    ASSERT_THROW(eval(parse("(+ 1 foo 2)")), scheme_error);
 }
 
-TEST(BuiltinTest, AbsNegativeInput) {
-    ASSERT_EQ(1, intValue(eval(parse("(abs -1)"))));
-}
+// -
 
-TEST(BuiltinTest, SubtractNoArgsThrows) {
+TEST(SubtractionTests, NoArgsThrows) {
     ASSERT_THROW(eval(parse("(-)")), scheme_error);
 }
 
-TEST(BuiltinTest, SubtractOneArgNegates) {
+TEST(SubtractionTests, OneArgNegates) {
     ASSERT_EQ(-1, intValue(eval(parse("(- 1)"))));
+}
+
+TEST(SubtractionTests, BasicSubtraction) {
+    ASSERT_EQ(1, intValue(eval(parse("(- 3 2)"))));
+}
+
+// abs
+
+TEST(AbsTests, AbsPositiveInput) {
+    ASSERT_EQ(1, intValue(eval(parse("(abs 1)"))));
+}
+
+TEST(AbsTests, AbsNegativeInput) {
+    ASSERT_EQ(1, intValue(eval(parse("(abs -1)"))));
 }
