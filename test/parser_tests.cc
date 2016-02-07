f218e415154cd97609d9fddc42b705abac35de30
Kyle Brown <picokyle@gmail.com>
2016-02-06 21:55:59 -0700

    test nested list parsing

--- test/parser_tests.cc
+++ test/parser_tests.cc
@@ -34,3 +34,13 @@ TEST(ParserTest, ParseOneElementList) {
     ASSERT_EQ(1, actual.size());
     ASSERT_EQ(3, boost::get<int>(actual.front()));
 }
+
+TEST(ParserTest, ParseNestedList) {
+    auto actual = boost::get<std::deque<SchemeExpr>>(parse("(length (1 2))"));
+    ASSERT_EQ(2, actual.size());
+    ASSERT_EQ("length", boost::get<std::string>(actual[0]));
+    auto sublist = boost::get<std::deque<SchemeExpr>>(actual[1]);
+    ASSERT_EQ(2, sublist.size());
+    ASSERT_EQ(1, boost::get<int>(sublist[0]));
+    ASSERT_EQ(2, boost::get<int>(sublist[1]));
+}
