6ee1590a9f2338ab1b5f2d97d36cada61d5f8fe2
Kyle Brown <picokyle@gmail.com>
2016-02-07 16:53:02 -0700

    only catch scheme_errors in REPL

--- src/repl.cc
+++ src/repl.cc
@@ -15,7 +15,7 @@ int main()
             std::cout << " * " << std::flush;
             getline(std::cin, line);
             if (std::cin) std::cout << eval(parse(line), env) << std::endl;
-        } catch (std::exception& e) {
+        } catch (const scheme_error& e) {
             std::cerr << "error: " << e.what() << std::endl;
         }
     } while (std::cin);
